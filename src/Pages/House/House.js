8c8055b64c1170fe6a419e681c0f729a3446268c
arifvtti <arif.vtti@gmail.com>
2022-11-12 09:14:52 +0600

    add search properties type option

---
+++ src/Pages/House/House.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const House = () => {
+    return (
+        <div class="card  bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default House;
---
+++ src/Pages/Houses/Houses.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import House from '../House/House';
+
+const Houses = () => {
+    const [houses, setHouses] = useState([]);
+
+    useEffect(() => {
+        fetch("house.json")
+            .then(res => res.json())
+            .then(data => setHouses(data))
+    }, [])
+    return (
+        <div>
+            {/* Search Section */}
+            <div className='my-10'>
+                <h1 className='text-3xl font-bold'>Search properties to rent</h1>
+                <div className='grid grid-cols-3 gap-10 mt-5'>
+                    <div class="card  bg-base-100 shadow-xl border">
+                        <div class="card-body">
+                            <h2 class="card-title">Location</h2>
+                            <select class="select select-bordered w-full max-w-xs">
+                                <option>Gaibandha, Bangladesh</option>
+                                <option>Gobindaganj, Bangladesh</option>
+                                <option>Bogra, Bangladesh</option>
+                                <option>Dhaka, Bangladesh</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card  bg-base-100 shadow-xl border">
+                        <div class="card-body">
+                            <h2 class="card-title">Price</h2>
+                            <select class="select select-bordered w-full max-w-xs">
+                                <option>&gt;1000</option>
+                                <option>&gt;1000, &lt;2000</option>
+                                <option>&gt;2000, &lt;3000</option>
+                                <option>&gt;3000</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card  bg-base-100 shadow-xl border">
+                        <div class="card-body">
+                            <h2 class="card-title">Type</h2>
+                            <select class="select select-bordered w-full max-w-xs">
+                                <option>House</option>
+                                <option>Flat</option>
+                                <option>Floor</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* All Properties */}
+
+        </div>
+    );
+};
+
+export default Houses;
